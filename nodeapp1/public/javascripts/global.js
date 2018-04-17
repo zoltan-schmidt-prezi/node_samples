f65888786c2b393e1afc03c9a15a80e76751a7d9
Zoltan Schmidt <zoltan.schmidt.a@gmail.com>
2018-04-17 15:40:59 +0200

    Populate table

---
+++ nodeapp1/public/javascripts/global.js
@@ -0,0 +1,36 @@
+// Rate data array for filling in info box
+var rateListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/rates', function( data ) {
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td>' + this.id + '</td>';
+            tableContent += '<td>' + this.name + '</td>';
+            tableContent += '<td>' + this.rate + '</td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML
+        // table
+        $('#rateList table tbody').html(tableContent);
+    });
+};
