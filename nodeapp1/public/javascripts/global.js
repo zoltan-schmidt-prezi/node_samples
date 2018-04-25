06828211eab47299dd0b0cef7a4003f1612576e0
EC2 Default User <ec2-user@ip-172-31-31-36.eu-central-1.compute.internal>
2018-04-25 09:09:47 +0000

    refactor finished, basic charting works

--- nodeapp1/public/javascripts/global.js
+++ nodeapp1/public/javascripts/global.js
@@ -1,34 +1,38 @@
-var queryDataDate = new Array();
-var queryDataRate = new Array();
-
-console.log("console");
 
 // DOM Ready =============================================================
 $(document).ready(function() {
 
-    // Populate the rate table on initial page load
+    // Populate the dropdown list on initial page load
     document.getElementById("tabletitle").textContent = "Rate table & charts";
     populateListItems();
-
 });
 
 // Dropdown selection change
 $('#sel_name').change(function() {
-    //populateMysqlTable( $(this).find('option:selected').attr('value') );
-    getOneBondDataFromServer( $(this).find('option:selected').attr('value') );
+    //Get the id of the selected bond in the dropdown
+    let selected_option =  $(this).find('option:selected').attr('value');
+
+    //Get all the data related to the selected bond
+    getOneBondDataFromServer( selected_option ).then( function(result) {
+        //populate the rate table on selection change
+        populateTable(result);
+        //Generate chart on x=date, y=rate
+        renderChart( getDataset(result, 'date'), getDataset(result, 'rate') );
+    });
 });
 
 // Functions =============================================================
 
 function populateListItems() {
-        $.getJSON('/list', function( data ) {
-            $.each(data, function(){
-                // Add dropdown list options with name and value
-                add_option("sel_name", this.name, this.id);
-            });
+    $.getJSON('/list', function( data ) {
+        $.each(data, function(){
+            // Add dropdown list options with name and value
+            add_option("sel_name", this.name, this.id);
         });
+    });
 }
 
+// Get all data for one bond from Database
 function getOneBondDataFromServer( bond_selected_ID ) {
     queryRateSeriesData = [];
     let promise = new Promise((resolve, reject) => {
@@ -46,27 +50,22 @@ function getOneBondDataFromServer( bond_selected_ID ) {
                 };
                 queryRateSeriesData.push(singleRateJSON);
             });
-            resolve("success");
+            resolve(queryRateSeriesData);
         });
     });
-    promise.then((successMessage) => {
-        populateTable(queryRateSeriesData);
-        return queryRateSeriesData;
-    });
+    return promise;
 }
 
 // Fill table with data
 function populateTable( bond_selected_JSON_array ) {
-    var mtableContent = '';
-    var tableTitle = '';
+    let mtableContent = '';
     
     //Set table title only once
-    tableTitle = bond_selected_JSON_array[0].name;
+    let = tableTitle = bond_selected_JSON_array[0].name;
     document.getElementById("tabletitle").textContent = "Rate table & charts for '" + tableTitle + "'";
     
     //Fill table with data
     for (i=0;i<bond_selected_JSON_array.length;i++) {
-        console.log(bond_selected_JSON_array[i].name); 
 
         mtableContent += '<tr>';
         mtableContent += '<td>' + bond_selected_JSON_array[i].date + '</td>';
@@ -75,23 +74,30 @@ function populateTable( bond_selected_JSON_array ) {
         mtableContent += '</tr>';
     }
     $('#mysqlRateList table tbody').html(mtableContent);
-    console.log("table populated")
 }
 
 function add_option(select_id, text, id) {
-    var select = document.getElementById(select_id);
+    let select = document.getElementById(select_id);
     select.options[select.options.length] = new Option(text, id);
 }
 
-function renderChart(){
+function getDataset( bond_selected_JSON_array, dataType ){
+    let dataSet = [];
+    for (i=0;i<bond_selected_JSON_array.length;i++) {
+        dataSet.push(bond_selected_JSON_array[i][dataType]);
+    }
+    return dataSet;
+}
+
+function renderChart(x_axis, y_axis){
     var ctx = document.getElementById("myChart");
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: queryDataDate, //date
+            labels: x_axis, //date
             datasets: [{
                 label: 'exchange rate',
-                data: queryDataRate, //rate
+                data: y_axis, //rate
                 borderColor: [
                     'rgba(255, 159, 64, 1)'
                 ],
@@ -110,7 +116,3 @@ function renderChart(){
     });
 }
 
-function clearChartData() {
-    queryDataDate = [];
-    queryDataRate = [];
-}
